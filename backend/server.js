d5e8fb167e65fa23a3253ebc15464b6d9ea2bfe0
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-06-21 22:22:36 +0300

    refactor server.js, configured and added routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-// const projectRoutes = require('./routes/ProjectRoutes');
-// const cardRoutes = require('./routes/CardRoutes');
-// const categoryRoutes = require('./routes/CategoryRoutes');
-// const taskRoutes = require('./routes/TaskRoutes');
+const projectRoutes = require('./routes/ProjectRoutes');
+const cardRoutes = require('./routes/CardRoutes');
+const categoryRoutes = require('./routes/CategoryRoutes');
+const taskRoutes = require('./routes/TaskRoutes');
 
 require('dotenv').config({ path: './config.env' });
 
@@ -14,10 +14,10 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors());
-// app.use('/api/projects', projectRoutes);
-// app.use('/api/cards', cardRoutes);
-// app.use('/api/categories', categoryRoutes);
-// app.use('/api/tasks', taskRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/cards', cardRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/tasks', taskRoutes);
 
 mongoose
   .connect(process.env.ATLAS_URI)
