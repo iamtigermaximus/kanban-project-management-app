882e2de4ef57af5ae80d103efbf8771ca65c369a
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-06-24 00:21:03 +0300

    created interfaces folder

--- frontend/src/components/projects-list/ProjectsList.tsx
+++ frontend/src/components/projects-list/ProjectsList.tsx
@@ -1,15 +1,31 @@
-import { initData } from '../../actions/initData';
 import {
   ProjectName,
   ProjectNameContainer,
   ProjectsListContainer,
 } from './ProjectsList.styles';
 import CreateNewBoard from '../modals/createNewBoard/CreateNewBoard';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { IProject } from '../../interfaces/Kanban';
 
 const ProjectsList = () => {
+  const [projects, setProjects] = useState<IProject[]>([]); // State variable to store the fetched projects
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5500/api/projects'); // Replace with your API endpoint
+      setProjects(response.data); // Handle the retrieved data
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // Call the function when the component mounts
+  }, []);
   return (
     <ProjectsListContainer>
-      {initData.map((project) => (
+      {projects.map((project) => (
         <ProjectNameContainer key={project.id}>
           <ProjectName>{project.name}</ProjectName>
         </ProjectNameContainer>
---
+++ frontend/src/interfaces/Kanban.ts
@@ -0,0 +1,31 @@
+export interface ILabel {
+  color: string;
+  text: string;
+}
+
+export interface ITask {
+  id: number;
+  completed: boolean;
+  text: string;
+}
+
+export interface ICard {
+  id: number;
+  title: string;
+  labels: ILabel[];
+  date: string;
+  tasks: ITask[];
+  desc?: string;
+}
+
+export interface ICategory {
+  id: number;
+  categoryTitle: string;
+  cards: ICard[];
+}
+
+export interface IProject {
+  id: number;
+  name: string;
+  data: ICategory[];
+}
